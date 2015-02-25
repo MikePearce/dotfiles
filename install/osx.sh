acdc8c5fbfd5ac9823915675043842c55138d6ba
Mike Pearce <mike@mikepearce.net>
2015-02-25 23:29:50 +0000

    Added vyprvpn to cask

--- install/osx.sh
+++ install/osx.sh
@@ -1,1 +1,1 @@
-brew cask install telegram spectacle firefox 1password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
+brew cask install vyprvpn telegram spectacle firefox 1password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
