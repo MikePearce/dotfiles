b94a0191aa98c683812686fd0efa09998505050a
Mike Pearce <mike@mikepearce.net>
2015-02-25 22:36:33 +0000

    Added telegram to cask

--- install/osx.sh
+++ install/osx.sh
@@ -1,1 +1,1 @@
-brew cask install flickr-uploader spectacle firefox 1password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
+brew cask install telegram spectacle firefox 1password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
