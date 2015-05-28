d61d28c771613cb180041122b900d1be5054360e
Mike Pearce <mike@mikepearce.net>
2015-05-28 13:59:17 +0100

    Mods to install and added unison stuff

--- install/osx.sh
+++ install/osx.sh
@@ -1,1 +1,5 @@
-brew cask install battle-net sequel-pro launchcontrol yacreader beardedspice qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package vyprvpn telegram spectacle firefox 1password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
+brew cask install vlc battle-net sequel-pro launchcontrol yacreader beardedspice qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package vyprvpn telegram spectacle firefox 2password kindle totalterminal transmit skitch sqwiggle scrivener evernote hipchat sublime-text dropbox insync skim minecraft google-chrome google-plus-auto-backup calibre android-file-transfer steam dropbox spotify
+
+# Install audio switcher
+git clone https://github.com/deweller/switchaudio-osx.git ~/bin/SwitchAudioSource
+echo 'Do not forget to install the "Audio Device.alfredworkflow" which you will find in this directory'
